082ee880dba236b96d0134d3b802322c9ab614ea
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-08-03 20:25:02 +0200

    Remove demo form

--- src/components/application/ApplicationPage.js
+++ src/components/application/ApplicationPage.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router'
-import Multistep from '../common/MultiStep'
-import Steps from './Steps/Steps'
+import ProjectForm from './Forms/ProjectForm'
 
 class ApplicationPage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <div>
-          <Multistep initialStep={1} steps={Steps}/>
-        </div>
-      </div >
+        <p>Bientôt votre candidature.</p>
+      </div>
     )
   }
 }
