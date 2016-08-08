56e161af920631e2b8fb45e20bb67908c8e2cc19
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-08-08 19:52:45 +0200

    Fix warning display in <App/> tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,13 +5,9 @@ import expect, { spyOn } from 'expect'
 import App from './App'
 
 describe('<App/>', () => {
-  // it('should be mounted once', () => {
-  //   const spy = spyOn(App.prototype, 'componentDidMount')
-  //   const wrapper = mount(<App/>)
-  //   expect(spy.calls.length).toEqual(1)
-  // })
   it('should have a Header', () => {
-    const wrapper = shallow(<App/>)
+    const children = <div/>
+    const wrapper = shallow(<App>{children}</App>)
     expect(wrapper.find('Header')).toExist()
   })
 })
