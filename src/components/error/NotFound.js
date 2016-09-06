cc62b02105507a6738c911ff21cbd21a19673a25
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-06 14:50:18 +0200

    Add basic 404 page

---
+++ src/components/error/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () =>
+  <div className="jumbotron">
+    <h3>404 page introuvable</h3>
+    <p>La page que vous cherchez n'existe pas.</p>
+  </div>
+
+export default NotFound
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import HomePage from './components/home/HomePage'
 import ApplicationPage from './components/application/ApplicationPage'
+import NotFound from './components/error/NotFound'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="application" component={ApplicationPage} />
+    <Route path="*" component={NotFound} />
   </Route>
 )
