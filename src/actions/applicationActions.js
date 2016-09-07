0a9b13f5f4240fafaf6f9325f72154679396ab08
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-07 14:29:07 +0200

    Add reducer and updates for profile and career

--- src/actions/applicationActions.js
+++ src/actions/applicationActions.js
@@ -2,6 +2,8 @@ import ApplicationApi from '../api/mockApplicationApi'
 import {loadContactSuccess} from './contactActions'
 import {loadProjectSuccess} from './projectActions'
 import {loadPepiteSuccess} from './pepiteActions'
+import {loadProfileSuccess} from './profileActions'
+import {loadCareerSuccess} from './careerActions'
 import * as types from './actionTypes'
 
 export function loadApplicationSuccess(application) {
@@ -19,6 +21,8 @@ export function loadApplication(id) {
       dispatch(loadContactSuccess(application.contact))
       dispatch(loadProjectSuccess(application.project))
       dispatch(loadPepiteSuccess(application.pepite))
+      dispatch(loadProfileSuccess(application.profile))
+      dispatch(loadCareerSuccess(application.career))
     }).catch(error => {
       throw (error)
     })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,16 @@ import application from './applicationReducer'
 import project from './projectReducer'
 import contact from './contactReducer'
 import pepite from './pepiteReducer'
+import career from './careerReducer'
+import profile from './profileReducer'
 
 const rootReducer = combineReducers({
   application,
   project,
   contact,
-  pepite
+  pepite,
+  career,
+  profile
 })
 
 export default rootReducer
