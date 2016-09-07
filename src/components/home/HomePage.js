77bf107935ee0b5375fb30de74011f0159304a61
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-07 10:50:35 +0200

    Use interpunct to feminize words

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {Link} from 'react-router'
 
 class HomePage extends React.Component {
-  componentDidMount() {}
+  componentDidMount() { }
   render() {
     return (
       <div className="jumbotron">
         <h1>Etudiant Entrepreneur</h1>
-        <p>Le statut national d'étudiant-entrepreneur permet aux étudiant(e)s et aux jeunes diplômé(e)s d'élaborer un projet entrepreneurial dans un PEPITE. Le diplôme d'établissement "étudiant-entrepreneur" (D2E) accompagne le statut d'étudiant-entrepreneur : il permet de mener à bien son projet avec un maximum de sécurité et de visibilité.</p>
+        <p>Le statut national d'étudiant-entrepreneur permet aux étudiant·e·s et aux jeunes diplômé·e·s d'élaborer un projet entrepreneurial dans un PEPITE.</p>
+        <p>Le diplôme d'établissement "étudiant-entrepreneur" (D2E) accompagne le statut d'étudiant-entrepreneur: il permet de mener à bien son projet avec un maximum de sécurité et de visibilité.</p>
         <Link to="application" className="btn btn-primary btn-lg">Demandez le statut</Link>
       </div>
     )
