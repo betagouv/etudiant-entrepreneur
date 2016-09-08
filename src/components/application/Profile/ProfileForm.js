6d00ada991e802e2d92bad8d80e1999fb14bfb68
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-08 16:11:07 +0200

    Fill basic profile page

--- src/components/application/Profile/ProfileForm.js
+++ src/components/application/Profile/ProfileForm.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react'
-import { FormGroup, ControlLabel, FormControl, Radio, HelpBlock } from 'react-bootstrap'
+import { FormGroup, ControlLabel, FormControl, Radio, HelpBlock, Panel } from 'react-bootstrap'
 import DatePicker from 'react-bootstrap-date-picker'
 import RadioGroup from '../../common/RadioGroup'
 import ValidatedFormControl from '../../common/ValidatedFormControl'
@@ -26,6 +26,62 @@ const ProfileForm = ({profile, contact, errors, onChange, onDateChange}) => {
         <ControlLabel>Date de naissance</ControlLabel>
         <DatePicker value={profile.birthDate} showYearDropdown onChange={onDateChange} dateFormatCalendar="MMMM" />
       </FormGroup>
+      <FormGroup className="required">
+        <ControlLabel>Lieu de naissance</ControlLabel>
+        <ValidatedFormControl name="birthPlace" type="text" placeholder="ville de naissance" onChange={onChange} value={profile.birthPlace} error={errors.birthPlace}/>
+      </FormGroup>
+      <FormGroup className="required">
+        <ControlLabel>Nationalité</ControlLabel>
+        <RadioGroup name="nationality" onChange={onChange} selectedValue={profile.nationality}>
+          <Radio value="fr">française</Radio>
+          <Radio value="other">étrangère</Radio>
+        </RadioGroup>
+      </FormGroup>
+      <FormGroup className={(profile.nationality == 'other') ? 'required' : 'required hidden'}>
+        <ControlLabel>Précisez</ControlLabel>
+        <ValidatedFormControl name="otherNationality" type="text" placeholder="nationalité" onChange={onChange} value={profile.otherNationality} error={errors.otherNationality}/>
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>Identifiant national étudiant (INE)</ControlLabel>
+        <ValidatedFormControl name="ine" type="text" placeholder="11 caractères" onChange={onChange} value={profile.ine} error={errors.ine}/>
+        <HelpBlock>identifiant ou numéro national étudiant unique présent sur ma carte d’étudiant·e ou mes relevés de notes.</HelpBlock>
+      </FormGroup>
+      <Panel header="Mes coordonnées">
+        <FormGroup className="required">
+          <ControlLabel>Adresse postale</ControlLabel>
+          <ValidatedFormControl name="address" type="text" placeholder="Adresse" onChange={onChange} value={profile.address} error={errors.address}/>
+        </FormGroup>
+        <FormGroup className="required">
+          <ControlLabel>Code postal</ControlLabel>
+          <ValidatedFormControl name="cp" type="text" placeholder="Code postal" onChange={onChange} value={profile.cp} error={errors.cp}/>
+        </FormGroup>
+        <FormGroup className="required">
+          <ControlLabel>Ville</ControlLabel>
+          <ValidatedFormControl name="city" type="text" placeholder="Ville" onChange={onChange} value={profile.city} error={errors.city}/>
+        </FormGroup>
+        <FormGroup className="required">
+          <ControlLabel>Pays</ControlLabel>
+          <ValidatedFormControl name="country" type="text" placeholder="Pays" onChange={onChange} value={profile.country} error={errors.country}/>
+        </FormGroup>
+      </Panel>
+      <Panel header="Mes réseaux sociaux">
+        <FormGroup>
+          <ControlLabel>mon twitter</ControlLabel>
+          <ValidatedFormControl name="twitter" type="url" placeholder="twitter" onChange={onChange} value={profile.twitter} error={errors.twitter}/>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>mon facebook</ControlLabel>
+          <ValidatedFormControl name="facebook" type="url" placeholder="facebook" onChange={onChange} value={profile.facebook} error={errors.facebook}/>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>mon linkedin</ControlLabel>
+          <ValidatedFormControl name="linkedin" type="url" placeholder="linkedin" onChange={onChange} value={profile.linkedin} error={errors.linkedin}/>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>mon viadeo</ControlLabel>
+          <ValidatedFormControl name="viadeo" type="url" placeholder="viadeo" onChange={onChange} value={profile.viadeo} error={errors.viadeo}/>
+        </FormGroup>
+      </Panel>
     </form>
   )
 }
