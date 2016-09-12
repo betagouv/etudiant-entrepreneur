2e325cbe6999f967143888f44dec3766842c1a28
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-13 00:24:49 +0200

    Fix indentation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-      <Header/>
+        <Header/>
         {this.props.children}
       </div>
     )
