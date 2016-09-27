fbb260d6a0e2000b93a6e436913ff64dccb125df
Hela <hela.ghariani@beta.gouv.fr>
2016-09-27 11:37:59 +0200

    Updat homepage

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -7,11 +7,10 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>Etudiant Entrepreneur</h1>
+        <h1>Deviens Etudiant Entrepreneur</h1>
+        <p>Tu as un projet ? Pour être aider et accompagner, rejoins la communauté.</p>
+        <Link to="application" className="btn btn-primary btn-lg">Demandez le statut</Link>  
         <BetaTestersPanel/>
-        <p>Le statut national d'étudiant-entrepreneur permet aux étudiant·e·s et aux jeunes diplômé·e·s d'élaborer un projet entrepreneurial dans un PEPITE.</p>
-        <p>Le diplôme d'établissement "étudiant-entrepreneur" (D2E) accompagne le statut d'étudiant-entrepreneur: il permet de mener à bien son projet avec un maximum de sécurité et de visibilité.</p>
-        <Link to="application" className="btn btn-primary btn-lg">Demandez le statut</Link>
       </div>
     )
   }
