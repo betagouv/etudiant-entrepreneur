36c97955a54805e6e0dca751e728464bae4edcfe
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-09-29 17:27:22 +0200

    Add printing view

---
+++ client/src/components/application/PrintPage.js
@@ -0,0 +1,66 @@
+import React, {PropTypes} from 'react'
+import toastr from 'toastr'
+
+import ProjectPage from './Project/ProjectPage'
+import TeamPage from './Team/TeamPage'
+import ContactPage from './Contact/ContactPage'
+import PepitePage from './Pepite/PepitePage'
+import SendPage from './Send/SendPage'
+import CareerPage from './Career/CareerPage'
+import ProfilePage from './Profile/ProfilePage'
+
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import * as errorsActions from '../../actions/errorsActions'
+import * as applicationActions from '../../actions/applicationActions'
+
+
+class ApplicationPage extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  componentDidMount() {
+    if (this.props.applicationId) {
+      this.props.applicationActions.loadApplication(this.props.applicationId).catch((err) => {
+        toastr.error(err)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <ContactPage/>
+        <TeamPage/>
+        <ProjectPage/>
+        <PepitePage/>
+        <CareerPage/>
+        <ProfilePage/>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    errors: state.errors,
+    applicationId: ownProps.params.id
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(errorsActions, dispatch),
+    applicationActions: bindActionCreators(applicationActions, dispatch)
+  }
+}
+
+ApplicationPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  applicationActions: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  applicationId: PropTypes.string
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ApplicationPage)
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,13 +3,14 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import HomePage from './components/home/HomePage'
 import ApplicationPage from './components/application/ApplicationPage'
+import PrintPage from './components/application/PrintPage'
 import NotFound from './components/error/NotFound'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="application" component={ApplicationPage} />
-    <Route path="application/:id" component={ApplicationPage} />
+    <Route path="application/:id/print" component={PrintPage}/>
+    <Route path="application(/:id)" component={ApplicationPage}/>
     <Route path="*" component={NotFound} />
   </Route>
 )
