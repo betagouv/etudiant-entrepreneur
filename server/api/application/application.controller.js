382c844cc5e5da4fb068df423a6b29bc68979cc3
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-10-13 17:14:32 +0200

    Add tutor email html body

--- server/api/application/application.controller.js
+++ server/api/application/application.controller.js
@@ -144,6 +144,26 @@ function getPepiteEmailBody(application) {
     '<p>Si tu as la moindre question, n\'hésites pas à nous contacter à contact@etudiant-entrepreneur.beta.gouv.fr</p>')
 }
 
+function getTutorEmailBody(application) {
+  return ('<html><body><p>Madame,Monsieur,</p>' +
+    '<br>' +
+    `<p>L'étudiant·e ${application.contact.firstname} ${application.contact.name} demande le statut national étudiant·e entrepreneur·e (SNEE)</p>` +
+    '<p>Nous vous informons de sa démarche en tant que référent.e pédagogique car l’obtention de ce statut a pour objet de rendre compatible études et projet d\'activités en proposant notamment des aménagements d\'emploi du temps, des crédits ECTS et la possibilité de substituer au stage le travail sur son projet.</p>' +
+    '<p>Il est dès lors important que les équipes pédagogiques puissent être informées le plus en maont possible et puissent être actrice dans ce processus.</p>' +
+    '<p>Le/la candidat·e a été informé·e que vous êtiez notifié·e de sa demande et est invité·e à prendre contact avec vous.</p>' +
+    '<p>Bien cordialement,<p>' +
+    '<p>L\'équipe de la plateforme en ligne.<p>' +
+    '<br>' +
+    '<div style=\'font-style: italic;\'>' +
+    '<p>Si vous souhaitez en savoir plus sur les PEPITE et le statut national étudiant·e entrepreneur·e, voici quelques liens utiles :</p>' +
+    '<ul><li><a href="http://www.enseignementsup-recherche.gouv.fr/cid102058/les-etudiants-entrepreneurs-optimistes-sur-leur-avenir.html" target="_blank">Un statut qui répond à la demande sociale</a></li>' +
+    '<li><a href="http://www.etudiant.gouv.fr/pid33854/entrepreneuriat-etudiant.html" target="_blank">Le SNEE</a></li>' +
+    '<li><a href="http://www.enseignementsup-recherche.gouv.fr/cid79223/pepite-poles-etudiants-pour-l-innovation-le-transfert-et-l-entrepreneuriat.html" target="_blank">Les PEPITE</a></li>' +
+    '<li><a href="http://www.pepite-france.fr/" target="_blank">PEPITE France</a></li></<ul>' +
+    '<li><a href="http://www.enseignementsup-recherche.gouv.fr/pid32602/faq-sur-statut-etudiant-entrepreneur-d2e.html" target="_blank">FAQ</a></li></<ul>' +
+    '</div>')
+}
+
 const pepites = [
   { id: '1', name: 'ETENA', email: 'a.latour@unistra.fr' },
   { id: '9', name: 'Bretagne', email: 'pepite-bretagne@u-bretagneloire.fr' },
