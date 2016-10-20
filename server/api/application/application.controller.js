5947a05db32f38679ac85f035aaf98589f9b85f4
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-10-20 12:09:22 +0200

    Remove tutor notification from graduates emails

--- server/api/application/application.controller.js
+++ server/api/application/application.controller.js
@@ -135,10 +135,14 @@ function getSaveEmailBody(application) {
 }
 
 function getSendEmailBody(application) {
+  var tutorInformed = ''
+  if (application.contact.situation == 'student') {
+    tutorInformed = '<p>Ton responsable pédagogique a été informé·e de ta candidature au statut, nous t’invitons à prendre contact avec lui.</p>'
+  }
   return ('<html><body><p>Bonjour,</p>' +
     `<p>Ta candidature a bien été envoyée au PEPITE ${getPepite(application.pepite.pepite).name} qui reviendra vers toi pour les prochaines étapes.</p>` +
     '<p>Ta  candidature passera en comité d\'engagement, la date de ce comité te sera donnée par ton PEPITE.</p>' +
-    '<p>Ton responsable pédagogique a été informé·e de ta candidature au statut, nous t’invitons à prendre contact avec lui.</p>' +
+    tutorInformed +
     '<a href="https://etudiant-entrepreneur.beta.gouv.fr/application/' + application._id + '" target="_blank">ta candidature</a>' +
     '<p>Si tu as la moindre question, n\'hésites pas à nous contacter à contact@etudiant-entrepreneur.beta.gouv.fr</p>' +
     '<p>Bonne aventure entreprenariale !</p>')
