1f1a97498d2d5a51d7448fe77e54d28a054fa864
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-11-02 10:21:57 +0100

    remove unused routes

--- server/api/pepite/pepite.controller.js
+++ server/api/pepite/pepite.controller.js
@@ -36,35 +36,6 @@ class PepiteController {
         return res.status(500).send(err)
       })
   }
-
-  createPepite(req, res) {
-    Pepite.create(req.body)
-      .then((pepite) => {
-        return res.status(201).json(pepite)
-      })
-      .catch((err) => {
-        req.log.error(err)
-        return res.status(500).send(err)
-      })
-  }
-
-  updatePepite(req, res) {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.sendStatus(404)
-    }
-    return Pepite
-      .findByIdAndUpdate(req.params.id, req.body, { new: true })
-      .then((pepite) => {
-        if (!pepite) {
-          return res.sendStatus(404)
-        }
-        return res.json(pepite)
-      })
-      .catch((err) => {
-        req.log.error(err)
-        return res.status(500).send(err)
-      })
-  }
 }
 
 module.exports = PepiteController
