ce8436a5e45c74ff8cc26b5397feb8ff396f0b2c
Nicolas Brohee <nicolas.brohee@gmail.com>
2016-11-30 18:26:40 +0100

    Set CORS_ORIGIN env variable for dev env

--- tools/devDeploy.sh
+++ tools/devDeploy.sh
@@ -3,4 +3,7 @@ git fetch --all
 git reset --hard origin/development
 git checkout development
 
+echo 'Set env variables'
+export CORS_ORIGIN=http://vps295137.ovh.net
+
 npm install && npm run start:build
