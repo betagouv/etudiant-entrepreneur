6a88b22fbfcfefbabef1185671b38d73d46cae9c
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-02-17 17:48:01 +0100

    Add application date to homepage

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import Footer from '../footer/Footer.js'
 import BetaTestersPanel from '../common/BetaTestersPanel'
+import { getCurrentUniversityYear } from '../common/yearHelper'
 
 class HomePage extends React.Component {
   componentDidMount() { }
@@ -12,7 +13,7 @@ class HomePage extends React.Component {
             <h1>Deviens Etudiant·e Entrepreneur·e</h1>
             <hr />
             <p className="header-sub-content">Tu as un projet ? Pour être aidé·e et accompagné·e, rejoins la communauté.</p>
-            <Link to="application" className="btn btn-primary btn-lg">Demande le statut</Link>
+            <Link to="application" className="btn btn-primary btn-lg">Demande le statut pour {getCurrentUniversityYear()}</Link>
           </div>
         <Footer className="footer"/>
       </div>
