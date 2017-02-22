01de6bf54935bd2d82677bc3357578cffc762448
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-02-22 14:19:47 +0100

    Set indexes on establishement's region and pepite

--- server/api/establishment/establishment.model.js
+++ server/api/establishment/establishment.model.js
@@ -6,8 +6,8 @@ var Schema = mongoose.Schema
 var EstablishmentSchema = new Schema({
   _id: Number,
   name: { type: String },
-  pepite: { type: Number, ref: 'Pepite' },
-  region: { type: Number, ref: 'Region' }
+  pepite: { type: Number, ref: 'Pepite', index: true },
+  region: { type: Number, ref: 'Region', index: true }
 })
 
 module.exports = mongoose.model('Establishment', EstablishmentSchema)
