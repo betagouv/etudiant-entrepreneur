c2f14032c0fb4d57beb2f1ed783778c24d61dc7a
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-03-16 17:37:24 +0100

    Add pagination on Pepite applicants

--- client/src/components/pepite/PepiteApplicantTable.js
+++ client/src/components/pepite/PepiteApplicantTable.js
@@ -1,28 +1,68 @@
 import React, { PropTypes } from 'react'
-import { Glyphicon } from 'react-bootstrap'
+import { Glyphicon, Pagination } from 'react-bootstrap'
 import PepiteApplicantRow from './PepiteApplicantRow'
 import TableHeader from '../common/Table/TableHeader'
 
-const PepiteApplicantTable = ({applicants, sort}) => {
-  return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <TableHeader onSort={sort} field="sentDate">Reçue depuis</TableHeader>
-          <TableHeader onSort={sort} field="contact.name">Nom</TableHeader>
-          <TableHeader onSort={sort} field="contact.firstname">Prénom</TableHeader>
-          <TableHeader onSort={sort} field="contact.email">Email</TableHeader>
-          <TableHeader onSort={sort} field="career.diploma.establishment">Établissement</TableHeader>
-          <th>Statut</th>
-          <th>Version imprimable</th>
-          <th>Comité d'engagement</th>
-        </tr>
-      </thead>
-      <tbody>
-        {applicants.map((application, i) => { return (<PepiteApplicantRow key={i} application={application} />) })}
-      </tbody>
-    </table>
-  )
+export class PepiteApplicantTable extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      activePage: 1,
+      itemsPerPage: 15
+    }
+    this.handlePageSelect = this.handlePageSelect.bind(this)
+  }
+
+  handlePageSelect(eventKey) {
+    this.setState({
+      activePage: eventKey
+    })
+  }
+
+  displayedItems() {
+    return this.props.applicants.slice(
+      (this.state.activePage - 1) * this.state.itemsPerPage,
+      (this.state.activePage) * this.state.itemsPerPage)
+  }
+
+  numberOfPages() {
+    return Math.ceil(this.props.applicants.length / this.state.itemsPerPage)
+  }
+
+  render() {
+    const { applicants, sort } = this.props
+    return (
+      <div>
+        <Pagination
+          prev
+          next
+          ellipsis
+          boundaryLinks
+          items={this.numberOfPages()}
+          maxButtons={5}
+          activePage={this.state.activePage}
+          onSelect={this.handlePageSelect} />
+
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <TableHeader onSort={sort} field="sentDate">Reçue depuis</TableHeader>
+              <TableHeader onSort={sort} field="contact.name">Nom</TableHeader>
+              <TableHeader onSort={sort} field="contact.firstname">Prénom</TableHeader>
+              <TableHeader onSort={sort} field="contact.email">Email</TableHeader>
+              <TableHeader onSort={sort} field="career.diploma.establishment">Établissement</TableHeader>
+              <th>Statut</th>
+              <th>Version imprimable</th>
+              <th>Comité d'engagement</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.displayedItems().map((application, i) => { return (<PepiteApplicantRow key={i} application={application} />) })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 PepiteApplicantTable.propTypes = {
