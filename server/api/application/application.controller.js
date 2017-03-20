162c915dfdd20eb229e59f101e1effb75ff3babb
nbrohee <nbrohee@users.noreply.github.com>
2017-03-20 17:52:39 +0100

    Escape quotes

--- server/api/application/application.controller.js
+++ server/api/application/application.controller.js
@@ -260,10 +260,10 @@ function getTutorEmailBody(application, pepite) {
     '<br>' +
     '<div style=\'font-style: italic;\'>' +
     '<p>Si vous souhaitez en savoir plus sur les PEPITE et le statut national étudiant·e entrepreneur·e, voici quelques liens utiles :</p>' +
-    '<ul><li><a href="http://www.etudiant.gouv.fr/pid33854/entrepreneuriat-etudiant.html" target="_blank">Qu'est-ce que le Statut National Étudiant Entrepreneur (SNEE)?</a></li>' +
+    '<ul><li><a href="http://www.etudiant.gouv.fr/pid33854/entrepreneuriat-etudiant.html" target="_blank">Qu\'est-ce que le Statut National Étudiant Entrepreneur (SNEE)?</a></li>' +
     '<li><a href="http://www.enseignementsup-recherche.gouv.fr/cid102058/les-etudiants-entrepreneurs-optimistes-sur-leur-avenir.html" target="_blank">Pourquoi un statut national Étudiant Entrepreneur ?</a></li>' +
     '<li><a href="http://www.enseignementsup-recherche.gouv.fr/cid79223/pepite-poles-etudiants-pour-l-innovation-le-transfert-et-l-entrepreneuriat.html" target="_blank">Qui sont les PEPITE ?</a></li>' +
-    '<li><a href="http://www.pepite-france.fr/" target="_blank">Qu'est-ce que le réseau PEPITE France ?</a></li></<ul>' +
+    '<li><a href="http://www.pepite-france.fr/" target="_blank">Qu\'est-ce que le réseau PEPITE France ?</a></li></<ul>' +
     '<li><a href="http://www.enseignementsup-recherche.gouv.fr/pid32602/faq-sur-statut-etudiant-entrepreneur-d2e.html" target="_blank">Et toutes les autres réponses à vos questions :)</a></li></<ul>' +
     '</div>')
 }
