e069170344ec456ff1afb33e725b90a9ec12d510
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-03-27 16:29:50 +0200

    Improve auth tests organization

--- server/api/auth/test/auth.api.test.js
+++ server/api/auth/test/auth.api.test.js
@@ -9,56 +9,70 @@ describe('api: auth', () => {
     app = new Server({ isTest: true }).getApp()
   })
 
-  describe('When requesting /api/auth/ with user missing', () => {
-    it('should return 401 given email and password are missing', (done) => {
-      supertest(app)
-        .post('/api/auth/')
-        .expect(401, { message: 'Missing credentials' }, done)
+  describe('When requesting /api/auth/', () => {
+    describe('When user is missing', () => {
+      it('should return 401', (done) => {
+        supertest(app)
+          .post('/api/auth/')
+          .expect(401, { message: 'Missing credentials' }, done)
+      })
     })
-    it('should return 401 given email and password are invalid', (done) => {
-      supertest(app)
-        .post('/api/auth/')
-        .send({ email: 'anonymous', password: 'wrongPassword' })
-        .expect(401, { 'message': 'Email ou mot de passe incorrect' }, done)
+    describe('When email and password are invalid', () => {
+      it('should return 401', (done) => {
+        supertest(app)
+          .post('/api/auth/')
+          .send({ email: 'anonymous', password: 'wrongPassword' })
+          .expect(401, { 'message': 'Email ou mot de passe incorrect' }, done)
+      })
     })
-    it('should return 401 given password is invalid', (done) => {
-      supertest(app)
-        .post('/api/auth/')
-        .send({ email: 'peel@univ-lorraine.fr', password: 'wrongPassword' })
-        .expect(401, { message: 'Email ou mot de passe incorrect' }, done)
+    describe('When password is invalid', () => {
+      it('should return 401', (done) => {
+        supertest(app)
+          .post('/api/auth/')
+          .send({ email: 'peel@univ-lorraine.fr', password: 'wrongPassword' })
+          .expect(401, { message: 'Email ou mot de passe incorrect' }, done)
+      })
     })
-    it('should return 200 and a token given email and password are valid', (done) => {
-      supertest(app)
-        .post('/api/auth/')
-        .send({ email: 'peel@univ-lorraine.fr', password: 'test' })
-        .expect(200)
-        .expect((res) => {
-          if (!('token' in res.body)) {
-            throw new Error('Missing token key')
-          }
-          validToken = res.body.token
-        })
-        .end(done)
+    describe('When email and password are valid', () => {
+      it('should return 200 and a token given', (done) => {
+        supertest(app)
+          .post('/api/auth/')
+          .send({ email: 'peel@univ-lorraine.fr', password: 'test' })
+          .expect(200)
+          .expect((res) => {
+            if (!('token' in res.body)) {
+              throw new Error('Missing token key')
+            }
+            validToken = res.body.token
+          })
+          .end(done)
+      })
     })
   })
 
   describe('When requesting route requiring authentication', () => {
-    it('should return 401 given no token is provided', (done) => {
-      supertest(app)
-        .get('/api/auth/authProtected')
-        .expect(401, done)
+    describe('When no token is provided', () => {
+      it('should return 401', (done) => {
+        supertest(app)
+          .get('/api/auth/authProtected')
+          .expect(401, done)
+      })
     })
-    it('should return 401 given an invalid token is provided', (done) => {
-      supertest(app)
-        .get('/api/auth/authProtected')
-        .set('Authorization', 'Bearer invalidToken')
-        .expect(401, 'The provided token is invalid', done)
+    describe('When an invalid token is provided', () => {
+      it('should return 401 ', (done) => {
+        supertest(app)
+          .get('/api/auth/authProtected')
+          .set('Authorization', 'Bearer invalidToken')
+          .expect(401, 'The provided token is invalid', done)
+      })
     })
-    it('should return 401 given an invalid token is provided', (done) => {
-      supertest(app)
-        .get('/api/auth/authProtected')
-        .set('Authorization', `Bearer ${validToken}`)
-        .expect(200, '"pong"', done)
+    describe('When a valid token is provided', () => {
+      it('should return 200', (done) => {
+        supertest(app)
+          .get('/api/auth/authProtected')
+          .set('Authorization', `Bearer ${validToken}`)
+          .expect(200, '"pong"', done)
+      })
     })
   })
 })
