ac70d72621c8888a3375608fd31a1589cb49994e
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-05-11 16:24:28 +0200

    plug debugmail to catch outgoing emails outside production

--- server/api/components/mail/sendMail.js
+++ server/api/components/mail/sendMail.js
@@ -2,13 +2,26 @@ const config = require('../../config').mail
 const nodemailer = require('nodemailer')
 const sgTransport = require('nodemailer-sendgrid-transport')
 
-exports.sendMail = function(email, done) {
-  const options = {
-    auth: {
-      api_key: config.apiKey
+exports.sendMail = function (email, done) {
+  let transport
+  if (process.env.NODE_ENV === 'production') {
+    transport = sgTransport({
+      auth: {
+        api_key: config.apiKey
+      }
+    })
+  } else {
+    transport = {
+      host: 'debugmail.io',
+      port: 25,
+      auth: {
+        user: process.env.DEV_MAIL_USER || '',
+        pass: process.env.DEV_MAIL_PASS || ''
+      }
     }
   }
-  const mailer = nodemailer.createTransport(sgTransport(options))
+
+  const mailer = nodemailer.createTransport(transport)
 
   mailer.sendMail(email, done)
 }
