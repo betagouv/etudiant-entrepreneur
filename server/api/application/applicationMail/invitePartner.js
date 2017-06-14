aeb8ec04bce693fb9a18f6c819765a493ead0c40
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-06-14 13:46:03 +0200

    Fix type in partner invite email

--- server/api/application/applicationMail/invitePartner.js
+++ server/api/application/applicationMail/invitePartner.js
@@ -10,7 +10,7 @@ function getMail(sender, application, pepite, partnerMail) {
 
 function getPartnerInviteEmailBody(application, pepite) {
   return ('<html><body><p>Bonjour,</p>' +
-    `<p>${application.contact.firstname} ${application.contact.name} a candidaté au <a target="_blank" href="http://www.enseignementsup-recherche.gouv.fr/cid79926/statut-national-etudiant-entrepreneur.html">status étudiant-entrepreneur</a> auprès du <a target="_blank" href="http://www.enseignementsup-recherche.gouv.fr/cid79223/pepite-poles-etudiants-pour-innovation-transfert-entrepreneuriat.html"><abbr title="Pôles Étudiants Pour l'Innovation, le Transfert et l'Entrepreneuriat">PEPITE</abbr></a> ${pepite.name} et t'a déclaré comme associé·e</p>` +
+    `<p>${application.contact.firstname} ${application.contact.name} a candidaté au <a target="_blank" href="http://www.enseignementsup-recherche.gouv.fr/cid79926/statut-national-etudiant-entrepreneur.html">statut étudiant-entrepreneur</a> auprès du <a target="_blank" href="http://www.enseignementsup-recherche.gouv.fr/cid79223/pepite-poles-etudiants-pour-innovation-transfert-entrepreneuriat.html"><abbr title="Pôles Étudiants Pour l'Innovation, le Transfert et l'Entrepreneuriat">PEPITE</abbr></a> ${pepite.name} et t'a déclaré comme associé·e</p>` +
     '<p>Si tu n\'as pas déjà déposé ta candidature, tu peux également le faire à cette adresse: etudiant-entrepreneur.beta.gouv.fr</p>' +
     '<p>Bonne journée.</p>' +
     '</body></html>')
