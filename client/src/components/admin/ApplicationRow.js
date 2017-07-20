2a6ea72f9b46df4a50095b10cf5d1e0990695d2d
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-07-20 16:47:15 +0200

    Add guard for establishmentName in AdminPage

--- client/src/components/admin/ApplicationRow.js
+++ client/src/components/admin/ApplicationRow.js
@@ -7,18 +7,22 @@ const ApplicationRow = ({ application, pepites }) => {
     application.pepite.pepite &&
     application.pepite.pepite != '0' &&
     Number(application.pepite.pepite) < pepites.length) ?
-  pepites[Number(application.pepite.pepite) - 1].name : '-'
+    pepites[Number(application.pepite.pepite) - 1].name : '-'
+
+  const establishmentName = (application.career && application.career.diploma) ?
+    application.career.diploma.establishment :
+    '-'
 
   return (
     <tr>
-      <td><TimeSinceControl textDate={application.sentDate}/></td>
+      <td><TimeSinceControl textDate={application.sentDate} /></td>
       <td>{application.contact.schoolYear}</td>
       <td>{application.contact.email}</td>
       <td>{application.contact.name}</td>
       <td>{application.contact.firstname}</td>
-      <td>{application.career.diploma.establishment}</td>
+      <td>{establishmentName}</td>
       <td>{pepiteName}</td>
-      <td><StatusLabel status={application.status}/></td>
+      <td><StatusLabel status={application.status} /></td>
     </tr>
   )
 }
