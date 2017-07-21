e8f07114469d626b7a01136b415b03ba5d8b4f8f
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-07-21 15:41:47 +0200

    Sort by sentDate on AdminPage

--- server/api/application/application.controller.js
+++ server/api/application/application.controller.js
@@ -42,6 +42,7 @@ class ApplicationController {
     return Promise.all([
       Application.find(findFilter).count(),
       Application.find(findFilter)
+        .sort({sentDate: 1})
         .skip(10 * page)
         .limit(10)
     ])
