0a691871490e1671812776d73f217df58df7e099
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-07-31 18:42:28 +0200

    Add application ellapsed time in transfer for new PEPITE

--- server/api/application/applicationMail/notifyPepiteNew.js
+++ server/api/application/applicationMail/notifyPepiteNew.js
@@ -1,3 +1,5 @@
+const Moment = require('moment')
+
 function getMail(sender, application, pepiteNew, pepiteOld) {
   return {
     from: sender,
@@ -9,9 +11,10 @@ function getMail(sender, application, pepiteNew, pepiteOld) {
 }
 
 function getPepiteEmailBody(application, pepiteOld) {
+  Moment.locale('fr')
   return ('<html><body><p>Bonjour,</p>' +
     `<p>La candidature de ${application.contact.firstname} ${application.contact.name} vous a été nouvellement transférée</p>` +
-    `<p>Elle était précédemment adressée au PEPITE ${pepiteOld.name}</p>` +
+    `<p>Elle a été envoyée il y a ${new Moment().utc(application.sentDate).fromNow(true)} et était précédemment adressée au PEPITE ${pepiteOld.name}</p>` +
     '<p>Vous retrouverez le dossier complet à l\'adresse :</p>' +
     '<a href="https://etudiant-entrepreneur.beta.gouv.fr/application/' + application._id + '" target="_blank">candidature</a>' +
     '<p>Pour rappel, la candidature est éditable à ce lien.</p>' +
