b7041e62ab5b809ed8001652df6f66da430e33f3
Nicolas Brohee <nicolas.brohee@gmail.com>
2017-08-07 12:13:43 +0200

    Remove map files from js dist

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -15,8 +15,6 @@ const GLOBALS = {
 }
 
 export default {
-  debug: true,
-  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
   entry: './src/index',
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
